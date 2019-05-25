a3c79d6892e36e545f7dfd86dea89405c85f9b24
suhailaben <40865556+suhailaben@users.noreply.github.com>
2019-05-25 13:16:01 -0700

    brows-info component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowsInfoComponent } from './brows-info/brows-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrowsInfoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/brows-info/brows-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brows-info',
+  templateUrl: './brows-info.component.html',
+  styleUrls: ['./brows-info.component.css']
+})
+export class BrowsInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
